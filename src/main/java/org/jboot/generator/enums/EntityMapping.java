46adad5f0a06fed2e9b1881d8611720d39d3160c
Aone <Aone@LAPTOP-TVQD8SLU>
2020-04-13 18:06:11 +0800

    修改代码

--- src/main/java/org/jboot/generator/enums/EntityMapping.java
+++ src/main/java/org/jboot/generator/enums/EntityMapping.java
@@ -28,7 +28,7 @@ public enum EntityMapping {
     /**
      * Identifies the generic SQL type {@code BIGINT}.
      */
-    BIGINT(Types.BIGINT, "Long", "BIGINT"),
+    BIGINT(Types.BIGINT, "BIGINT", "Long"),
     /**
      * Identifies the generic SQL type {@code FLOAT}.
      */
